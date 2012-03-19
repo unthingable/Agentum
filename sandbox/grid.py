49dbe166aa455d96c8848f4f86ffa68b34de542f
dev <dev@dev.localdomain>
2012-03-19 11:54:51 -0500

    xrange

--- sandbox/grid.py
+++ sandbox/grid.py
@@ -11,17 +11,21 @@ class Grid(object):
     N-dimensional rectangular grid.
     """
 
-    def __init__(self, dimensions=(100,100)):
+    def __init__(self, dimensions=(100,100), names=None, cell_fn=Cell):
         """
-        dimensions: list of dimensions.
+        dimensions: list of dimensions
+        names:      optional list of dimension names
+        cell_fn:    a function that returns a new cell instance
+
         Ex. (100,100,20) for a 3-dimensional 100x100x20 grid.
         """
         self._dimensions = dimensions
+        self._dimension_names = names
         """
         The grid is a dict keyed by coordinate tuple and valued by the Cell
         object (for now).
         """
-        self.cells = dict((tuple(xyz), Cell(0)) for xyz in product(*map(range, dimensions)))
+        self.cells = dict((tuple(xyz), cell_fn()) for xyz in product(*map(xrange, dimensions)))
 
     def __getitem__(self, xyz): return self.cells[xyz]
 

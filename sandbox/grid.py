f9a02ad575f070095e426b667458c82e9519ea2b
Alex Kouznetsov <dev@mydeb.(none)>
2012-03-28 12:34:43 -0400

    grid.clear, refactor

--- sandbox/grid.py
+++ sandbox/grid.py
@@ -14,28 +14,35 @@ def memoize(f):
     return decorated_function
 
 class Cell(object):
-    __slots__ = "heat", "bug"
+    __slots__ = "heat", "bug", "_init"
 
     def __init__(self, heat=0, bug=None):
         self.heat = heat
         self.bug = bug # only one bug allowed
+        self._init = dict(heat=heat,bug=bug)
+
+    def clear(self):
+        self.heat = 0
+        self.bug = None
+
+    def reset(self):
+        self.__init__(**self._init)
 
 class CellSpace(object):
     """
-    In a cellular (discreet) space agents can look around and move.
+    In a cellular (discrete) space agents can look around and move.
     """
     @abstractmethod
-    def cells(self, traverse=None):
-        # return cell iter
-        pass
+    def cells(self, traverse=None): pass
 
     @abstractmethod
-    def neighbors(self, cell, r=1):
-        pass
+    def neighbors(self, cell, r=1): pass
 
     @abstractmethod
-    def distance(self, cell, r=1):
-        pass
+    def distance(self, cell, r=1): pass
+
+    @abstractmethod
+    def clear(self): pass
 
     @staticmethod
     def tr_random(cells):
@@ -53,7 +60,7 @@ class Grid(CellSpace):
     CellSpace implemented as N-dimensional rectangular grid.
     """
 
-    def __init__(self, dimensions=(100,100), names=None, cell_fn=Cell):
+    def __init__(self, dimensions=(100,100), names=None, cell_class=Cell):
         """
         dimensions: list of dimensions
         names:      optional list of dimension names
@@ -63,10 +70,15 @@ class Grid(CellSpace):
         """
         self._dimensions = dimensions
         self._dimension_names = names
+        self.cell_class = cell_class
         # The grid is a dict keyed by coordinate tuple and valued by the Cell
         # object (for now).
-        self.cell_map = dict((tuple(xyz), cell_fn()) for xyz in product(*imap(xrange, dimensions)))
+        self.cell_map = dict((tuple(xyz), cell_class()) for xyz in product(*imap(xrange, dimensions)))
         self.inverted_cell_map = dict((v,k) for k,v in self.cell_map.iteritems())
+
+    def clear(self):
+        map(cell_class.clear, self.cells())
+        # neighbors do not change, so neighbors() can stay memoized with a prefilled cache
 
     def __getitem__(self, xyz): return self.cell_map[xyz]
 
@@ -82,6 +94,7 @@ class Grid(CellSpace):
     def __len__(self):
         return len(self.cell_map)
 
+    # without memoization this can be a little slow
     def _get_neighbor_indexes(self, cell, r=1):
         xyz = self.inverted_cell_map[cell]
         ranges = (xrange(x-r,x+r+1) for x in xyz)

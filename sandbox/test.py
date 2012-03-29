a37cba78f85b2fcec8e0d641e6e78c00bb4e0247
Alex Kouznetsov <dev@mydeb.(none)>
2012-03-29 14:37:37 -0400

    Add tests

---
+++ sandbox/test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from grid import GridSpace
+import sim
+
+class GridTest(unittest.TestCase):
+    def setUp(self):
+        class Cell(object):
+            def __init__(self):
+                self.heat = 0
+        self.g = GridSpace((100,100), cell_fn=Cell)
+
+    def testCreate(self):
+        GridSpace()
+
+    def testCreateWithProperties(self):
+        self.assertEquals(self.g[0,0].heat, 0)
+
+class SimTest(unittest.TestCase):
+    def setUp(self):
+        self.g = GridSpace(cell_fn=sim.BugCell)
+        self.bugs = sim.init_grid(self.g, numbugs=20)
+
+    def testSimStep(self):
+        for bug in self.bugs:
+            sim.step_bug(self.g, bug)
+
+if __name__=="__main__":
+    unittest.main()

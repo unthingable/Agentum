948e1cf591b6c6f70d828a9385471381f50ec1b4
alkouznetsov <alkouznetsov@ebay.com>
2012-08-25 22:37:19 -0700

    touching up, WIP

--- agentum/simulation.py
+++ agentum/simulation.py
@@ -9,10 +9,17 @@ class Simulation(object):
     agents = []
     metaagents = []
 
+    # Need a more elegant way of doing this...
+    # Put your runtime stuff in here
+    runtime = None
+
     @property
     def config():
         '''
         Return simulation config
         '''
 
-
+# A smart container to propagate properties.
+# Anything you want shared between agents goes here.
+class Container(object):
+    pass

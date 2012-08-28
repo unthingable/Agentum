a72cbe25bfcf10eff0aa54286aad004c89b24d4e
dev <dev@mydeb.(none)>
2012-08-28 03:39:39 -0400

    runner: prepping for serious modularity

--- agentum/runner.py
+++ agentum/runner.py
@@ -8,7 +8,9 @@ import logging
 import optparse
 from agentum.simulation import Simulation
 
-log = logging.Logger(__name__)
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
 def parse_args():
     usage = "usage: %prog [options] [file]"
@@ -17,12 +19,19 @@ def parse_args():
     parser.add_option("-s", "--steps", dest="steps", default=100,
         help="The number of simulation steps"
     )
+    parser.add_option("--no-wait", dest="nowait", default=False,
+        help="Run the simulation instead of waiting for remote control"
+    )
 
     options, args = parser.parse_args()
 
     return options, args
 
-def run(module, steps):
+def run_main():
+    options, args = parse_args()
+    simmodule = args[0]
+    module = load_module(simmodule
+                         )
     setup = getattr(module, 'setup', None)
     if not setup:
         raise Exception("No setup() found in module %s" % module.__name__)
@@ -30,26 +39,33 @@ def run(module, steps):
     sim = Simulation()
     setup(sim)
 
-    log.info("Running simulation %s for %d steps..." % (module.__name__, steps))
-    # Much optimization todo
-    for step in range(steps):
-        log.debug("Step: %d" % step)
-        # Run agents
-        for agent in sim.agents:
-            agent.run(sim)
-        # Run metaagents
-        for cell in sim.space.cells():
-            for metaagent in sim.metaagents:
-                metaagent.run(sim, cell)
-        # This is a good place to emit state updates and such
+    if options.nowait:
+        log.info("Running simulation %s for %d steps..." % (module.__name__, steps))
+        # Much optimization todo
+        for step in range(steps):
+            log.debug("Step: %d" % step)
+            # Run agents
+            for agent in sim.agents:
+                agent.run(sim)
+            # Run metaagents
+            for cell in sim.space.cells():
+                for metaagent in sim.metaagents:
+                    metaagent.run(sim, cell)
+            # This is a good place to emit state updates and such
+    else:
+        # Load ZMQ control center and start the wait loop
+        log.info("Simulation loaded, waiting for control (not yet implemented)")
+        import time
+        while True:
+            time.sleep(1)
 
-def run_main():
+def load_module(simmodule):
     options, args = parse_args()
     simmodule = args[0]
     if os.path.isfile(simmodule):
         dirname, module_name = os.path.split(simmodule)
         module_name = module_name.replace('.py','')
         module = imp.load_source(module_name, simmodule)
-        run(module, options.steps)
     else:
-        print "Not a file: %s" % simmodule
+        raise Exception("Not a file: %s" % simmodule)
+    return module

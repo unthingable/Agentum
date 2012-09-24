94a22f4a898fbf36105768fddccda74fe145aa74
dev <dev@mydeb.(none)>
2012-09-24 02:28:47 -0400

    Fix simulation globals, now simulation can restart

--- agentum/simulation.py
+++ agentum/simulation.py
@@ -6,19 +6,10 @@ from protocol import Propagator
 
 class Simulation(Propagator):
 
-    space = None    # A single "physical" space for now.
-    networks = {}
-    agents = []
-    metaagents = []
-
-    # Need a more elegant way of doing this...
-    # Put your runtime stuff in here
-    runtime = None
-
     stream_name = 'sim'
 
-    @property
-    def config():
-        '''
-        Return simulation config
-        '''
+    def __init__(self):
+        self.space = None    # A single "physical" space for now.
+        #networks = {}
+        self.agents = []
+        self.metaagents = []

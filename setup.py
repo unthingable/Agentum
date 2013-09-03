6eae03505b7013e383550259f539ad91c456cd7d
AlexK <alex@eat-up.org>
2013-09-03 09:58:45 -0700

    growing up

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup
+import os
+
+from agentum import __version__
+
+
+def local_file(fn):
+    return open(os.path.join(os.path.dirname(__file__), fn))
+
+
+with local_file('requirements.txt') as f:
+    requirements = map(str.strip, f)
+
 
 setup(
     name="Agentum",
-    description="Multiagent modeling toolkit",
-    version="0.1",
+    description="Agent-based modeling toolkit",
+    version=__version__,
     author="Alex Kouznetsov, Max Tsvetovat",
     packages=['agentum'],
-    install_requires=[
-        'setuptools',
-        'simplejson',
-#        'mockcache',
-#        'tornado',
-#        'pyzmq',
-        'greenlet',
-        'gevent',  # libevent-dev
-#        'gevent-zeromq',
-        'gevent-websocket',
-    ],
+    install_requires=requirements,
     entry_points="""
         [console_scripts]
         agentum = agentum.runner:run_main
